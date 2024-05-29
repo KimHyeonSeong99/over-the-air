9e56dd70647c13fbc631d6c38ed5f85e013da399
hyeonsoung <gj0690@kookmin.ac.kr>
2024-05-29 12:57:40 +0900

    update subscriber_firmware.py

--- subscriber_firmware.py
+++ subscriber_firmware.py
@@ -8,8 +8,8 @@ import numpy as np
 from PIL import Image
 import base64
 import io
-import time  # Add this line to import the time module
-import matplotlib.pyplot as plt
+import time
+from tkinter import *
 
 Sub_Topic = "updates/firmware" 
 userId = "Alice"
@@ -29,6 +29,12 @@ def compute_file_hash(file_path):
             sha256_hash.update(chunk)
     return sha256_hash.hexdigest()
 
+def event_PB_Now():
+    global choice
+    choice = 'yes'
+    
+
+
 # subscriber callback
 def on_message(client, userdata, msg):
 
@@ -47,21 +53,31 @@ def on_message(client, userdata, msg):
     if float(version[FileName]) < float(FirmwareVersion):
         with open(firmwarePath,"w") as stream:
             stream.write(FirmwareData)
-        download_firmware = input("Do you want to download the new firmware?[y/n]: ")
-        if download_firmware.lower() == 'y':
-            try:
-                firmwareDirectory = '/home/sea/sea-me-hackathon-2023/Cluster/src/'
-                firmwarePath = os.path.join(firmwareDirectory, FileName)
-                with open(firmwarePath,"w") as stream:
-                    stream.write(FirmwareData) 
-                time.sleep(1)
-                print(f"Firmware downloaded and saved to {firmwareDirectory}")
-            except Exception as e:
-                print(f"Error downloading the firmware: {e}")
-                print(f"firmwareDirectory: {firmwareDirectory}")
-                print(f"firmwarePath: {firmwarePath}")
-        else:
-            print("Deny downloading new firmware")
+        
+        later_time = IntVar()
+        later_time = 1
+        while(later_time)
+            OTA_UI = Tk()
+            OTA_UI.title("Choice update")
+            information = Label(OTA_UI,text = 'Do you want to update new firmware?\nclick the button what you want',font = (20,'bold'))
+            button_Submit = Button(OTA_UI,text = 'Submit',command = event_PB)
+            Later_time0 = Radiobutton(OTA_UI, text = 'Now', value = 0, variable = later_time)
+            Later_time1 = Radiobutton(OTA_UI, text = '10min', value = 600, variable = later_time)
+            Later_time1 = Radiobutton(OTA_UI, text = '1hour', value = 3600, variable = later_time)
+            Later_time1 = Radiobutton(OTA_UI, text = '1day', value = 86400, variable = later_time)
+            Later_time1 = Radiobutton(OTA_UI, text = '1week', value = 509000, variable = later_time)
+
+        try:
+            firmwareDirectory = '/home/sea/sea-me-hackathon-2023/Cluster/src/'
+            firmwarePath = os.path.join(firmwareDirectory, FileName)
+            with open(firmwarePath,"w") as stream:
+                stream.write(FirmwareData) 
+            time.sleep(1)
+            print(f"Firmware downloaded and saved to {firmwareDirectory}")
+        except Exception as e:
+            print(f"Error downloading the firmware: {e}")
+            print(f"firmwareDirectory: {firmwareDirectory}")
+            print(f"firmwarePath: {firmwarePath}")
 
         os.chdir("/home/sea/sea-me-hackathon-2023/Cluster/src")
         os.system("make -j6")
