aa8ee3d7328183930385eff249cedc895152dfb2
hyeonsoung <101382350+hyeonsoung@users.noreply.github.com>
2024-05-31 17:58:50 +0900

    Add files via upload

--- client_listener.py
+++ client_listener.py
@@ -1,6 +1,10 @@
-
+import json
 import os
 import socket
+import base64, cv2, io
+from PIL import Image
+import numpy as np
+
 file_path = '/home/sea/sea-me-hackathon-2023/Cluster/src/'
 
 def receive_file(server_host, server_port, buffer_size=4096):
@@ -10,19 +14,31 @@ def receive_file(server_host, server_port, buffer_size=4096):
         print(f"Server listening on {server_host}:{server_port}")
         
         conn, addr = server_socket.accept()
-        file_name = conn.recv()
         with conn:
+            message = ""
             print(f"Connected by {addr}")
-            with open(file_path + file_name, "wb") as f:
-                while True:
-                    data = conn.recv(buffer_size)
-                    if not data:
-                        break
-                    f.write(data)
+            while True:
+                data = conn.recv(buffer_size).decode('utf-8')
+                if not data:
+                    break
+                message += data
+            message = json.loads(message)
+            file_name = message['FileName']
+            if file_name.split('.')[-1] == 'png':
+                image = message['image']
+                decode_img = base64.b64decode(image.encode())
+                img_out = Image.open(io.BytesIO(decode_img))
+                img_array = np.array(img_out)
+                img = cv2.cvtColor(img_array,cv2.COLOR_BGR2RGB)
+                cv2.imwrite(file_path + 'image/' + file_name, img)
+            else:
+                firmware = message['firmware']
+                with open(file_path + file_name, "w") as f:
+                    f.write(content)
             print("File received successfully.")
 
 
-server_host = "0.0.0.0"  # Listen on all interfaces
+server_host = "192.168.1.170"  # Listen on all interfaces
 server_port = 12345
 
 while True:
