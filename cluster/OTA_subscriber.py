22ae7e0ca28ac68fb3b775d091d50a59256a25fb
hyeonsoung <101382350+hyeonsoung@users.noreply.github.com>
2024-06-11 18:25:35 +0900

    Update OTA_subscriber.py

--- cluster/OTA_subscriber.py
+++ cluster/OTA_subscriber.py
@@ -4,6 +4,7 @@ import os, cv2, base64
 import time
 from tkinter import *
 from tkinter import messagebox
+from PIL import Image
 import socket
 import numpy as np
 
@@ -11,15 +12,16 @@ timelist = {'Now':0,'10min':600,'1hour':3600,'1day':86400,'1week':604800}
 Sub_Topic = "updates/" 
 userId = "Alice"
 userPw = "mose"
-brokerIp = "203.246.114.226"
+brokerIp = "mosquitto broker ip"
 port = 1883
-server_host = "remote_host_ip_or_domain"
+server_host = "client socket ip"
 server_port = 12345
-tmpDirectory = "C:/Users/user/OTA/tmp/"
+tmpDirectory = "tmpdir"
+versionPath = "version.json"
 
 def on_connect(client, userdata, flags, rc):
     print("Connected with result code " + str(rc))
-    with open(tmpDirectory + 'version.json', 'r') as version_json:
+    with open(versionPath, 'r') as version_json:
         version = json.load(version_json)
     for key in version.keys():
         client.subscribe(Sub_Topic+ key)
@@ -64,21 +66,17 @@ def update_choice():
     OTA_UI.mainloop()
     return timelist.get(later_time.get())
 
-def send_file(server_host, server_port, file_path, buffer_size=4096):
+def send_file(server_host, server_port, message):
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as client_socket:
         client_socket.connect((server_host, server_port))
-        print(f"Connected to server {server_host}:{server_port}")
-        client_socket.sendall(file_path.split('/')[-1])
-        with open(file_path, "rb") as f:
-            while (data := f.read(buffer_size)):
-                client_socket.sendall(data)
+        print(f"Connected to server {server_host}:{server_port}")       
+        client_socket.sendall(json.dumps(message).encode('utf-8'))
         print("File sent successfully.")
 
 # subscriber callback
 def on_message(client, userdata, msg):
 
     try:
-        versionPath = os.path.join(tmpDirectory,"version.json")
         with open(versionPath,"r") as versionlist:
             version = json.load(versionlist) 
     except:
@@ -105,6 +103,7 @@ def on_message(client, userdata, msg):
                 print(f"firmwareDirectory: {tmpDirectory}")
                 print(f"firmwarePath: {firmwarePath}")
         else:
+            firmwarePath = os.path.join(tmpDirectory, FileName)
             ImageData = payload['image'].encode()
             decode_img = base64.b64decode(ImageData)
             img_out = Image.open(io.BytesIO(decode_img))
@@ -116,20 +115,20 @@ def on_message(client, userdata, msg):
         while(flag):
             flag = update_choice()
             time.sleep(flag)
-        send_file(server_host, server_port, firmwarePath)
-        version[FileName] = FileVersion
-        with open(versionPath,"w") as versionlist:
-            versionlist.write(json.dumps(version))
-
+        flag = 1
+        while(flag):
+            try:
+                send_file(server_host, server_port, payload)
+                version[FileName] = FileVersion
+                flag = 0
+            except:
+                print("Cluster and Controller are not conected! \nRetry send file")
+                time.sleep(60)
+                
     else:
-        print("="*50)
-        print("File name: " + FileName)
-        print("File version: " + FileVersion)
-        print("Latest version: " + version[FileName])
         print("new firmware version is old version, pass the update")
-        print("="*50)
 
-
+    
     print("Ready for a new update")
 
 
@@ -141,7 +140,6 @@ client.on_message = on_message
 client.connect(brokerIp, port, 60)
 
 client.loop_forever()
-
 
 
 
