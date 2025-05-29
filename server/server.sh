ae7f747926e32f97b5c8eedd3b38030f735cc193
hyeonsoung <gj0690@kookmin.ac.kr>
2025-05-29 14:18:47 +0900

    Update server.sh

--- server/server.sh
+++ server/server.sh
@@ -1,19 +1,5 @@
 #!/bin/bash
 
-#install mosquitto
-sudo apt-get update
-sudo apt-get install mosquitto -y
-
-sudo mosquitto_passwd -b /etc/mosquitto/pwfile mose mose
-sudo bash -c 'cat >> /etc/mosquitto/mosquitto.conf <<EOF
-listener 1883 0.0.0.0
-allow_anonymous false
-password_file /etc/mosquitto/pwfile
-EOF
-'
-
-sudo systemctl restart mosquitto
-
 #install paho-mqtt
 sudo apt-get install python3-paho-mqtt -y
 
