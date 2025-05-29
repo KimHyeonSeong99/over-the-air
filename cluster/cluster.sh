6b53bd685004bbe05ac0d3d07ee8980b0652af2f
hyeonsoung <gj0690@kookmin.ac.kr>
2025-05-29 14:01:43 +0900

    modify sh

--- cluster/cluster.sh
+++ cluster/cluster.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+
+sudo apt-get update
+sudo apt-get install python3-can -y
+
 cd "$(dirname "$0")"  # sh 파일이 있는 디렉토리로 이동
 sudo ip link set can0 up type can bitrate 1000000
 python3 cluster.py
