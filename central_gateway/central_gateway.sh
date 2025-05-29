60e5b014f7eb10b3baeb6d6589537f5863fa0ce7
hyeonsoung <gj0690@kookmin.ac.kr>
2025-05-29 13:55:38 +0900

    Update central_gateway.sh

--- central_gateway/central_gateway.sh
+++ central_gateway/central_gateway.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
-cd "$(dirname "$0")"  # sh 파일이 있는 디렉토리로 이동
+
+#install paho-mqtt
+sudo apt-get install python3-paho-mqtt -y
+
+cd "$(dirname "$0")"
 sudo ip link set can0 up type can bitrate 1000000
 python3 central_gateway.py
 sudo ip link set can0 down
