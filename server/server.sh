db76d72e0b6cafb2d901ae18720865637f2c3164
hyeonsoung <gj0690@kookmin.ac.kr>
2025-05-29 13:20:54 +0900

    Update server.sh

--- server/server.sh
+++ server/server.sh
@@ -22,5 +22,6 @@ sudo apt-get install python3-flask -y
 
 #start server
 cd "$(dirname "$0")"  # Move to the directory where the script is located
-python3 server_notice.py
-python3 server_download.py
+python3 server_notice.py &
+python3 server_download.py &
+wait
